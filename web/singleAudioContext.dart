25a7b0394e51cc24b163f0efe4a11733ebfcef84
ne4istb <anechytailov@gmail.com>
2014-03-03 21:33:45 +0200

    Implemented pause/continue

--- web/singleAudioContext.dart
+++ web/singleAudioContext.dart
@@ -5,51 +5,42 @@ import 'dart:async';
 import 'dart:typed_data';
 
 class SingleAudioContext {
-  
+
   static AudioContext _audioContext = new AudioContext();
-  static final SingleAudioContext _singleton = new SingleAudioContext._internal();
+  static final SingleAudioContext _singleton = new SingleAudioContext._internal(
+      );
 
   factory SingleAudioContext() {
     return _singleton;
   }
-  
+
   SingleAudioContext._internal();
-  
+
   AudioDestinationNode get destination => _audioContext.destination;
-  
-  Future<AudioBuffer> decodeAudioData(ByteBuffer audioData){
+  double get currentTime => _audioContext.currentTime;
+
+  Future<AudioBuffer> decodeAudioData(ByteBuffer audioData) {
     return _audioContext.decodeAudioData(audioData);
   }
-  
-  List<AudioBufferSourceNode> currentSources = [];
-  
+
+  List<AudioBufferSourceNode> _currentSources = [];
+
   var startTime = 0;
-  void playFromBuffer (AudioBuffer buffer, {num startTime: 0}){
+  void playFromBuffer([AudioBuffer buffer, num startTime, num offset]) {
     AudioBufferSourceNode source = _audioContext.createBufferSource()
         ..buffer = buffer
         ..connectNode(_audioContext.destination)
-        ..start(_audioContext.currentTime + startTime);
-    
-    currentSources.add(source);
+        ..start(_audioContext.currentTime + startTime, offset);
+
+    _currentSources.add(source);
   }
 
-  void stopAll(){
-    currentSources.forEach(_stopSource);   
-    currentSources = [];
+  void stopAll() {
+    _currentSources.forEach(_stopSource);
+    _currentSources = [];
   }
-  
-  void _stopSource(AudioBufferSourceNode source){
+
+  void _stopSource(AudioBufferSourceNode source) {
     source.stop(0);
-  }
-  
-  var startOffset = 0;
-  
-  
-  void pause(){
-
-    startOffset += _audioContext.currentTime - startTime;
-    
-    currentSources.forEach((source) { source.stop(); });   
-    startOffset += _audioContext.currentTime - startTime;
   }
 }
--- web/trackLine/trackLine.dart
+++ web/trackLine/trackLine.dart
@@ -5,60 +5,56 @@ import 'dart:html';
 import 'dart:convert' show JSON;
 
 @NgComponent(
-    selector: 'track',
-    templateUrl: 'trackLine/trackLine.html',
-    cssUrl: 'trackLine/trackLine.css',
-    publishAs: 'cmp'
-)
+    selector: 'track', 
+    templateUrl: 'trackLine/trackLine.html', 
+    cssUrl: 'trackLine/trackLine.css', 
+    publishAs: 'cmp')
 class TrackLineComponent {
   @NgAttr('number')
   String number;
-  
+
   String get label => "Track " + number;
-  
+
   @NgTwoWay('cells')
   List<TrackLineCell> cells;
-  
+
   bool _clearCellAllowed = false;
-  void drop(MouseEvent e, int index){
-    
-    if (cells[index] != null)
-      return;
-    
+  void drop(MouseEvent e, int index) {
+
+    if (cells[index] != null) return;
+
     String id = e.dataTransfer.getData('Id');
-    if (id != (e.currentTarget as Element).id)
-      _clearCellAllowed = true;
-    
+    if (id != (e.currentTarget as Element).id) _clearCellAllowed = true;
+
     String sampleName2 = e.dataTransfer.getData('SampleName');
     String sampleHref = e.dataTransfer.getData('SampleHref');
-    
-    cells[index]= new TrackLineCell(sampleName2, sampleHref);
+
+    cells[index] = new TrackLineCell(sampleName2, sampleHref);
   }
-  
+
   bool _ctrlPressed;
-  void allowDrop(MouseEvent e){
+  void allowDrop(MouseEvent e) {
     _ctrlPressed = e.ctrlKey;
     e.preventDefault();
   }
-  
-  void onItemDragged(int index){
-    if (!_ctrlPressed && _clearCellAllowed)
-      clearCell(index);
-    
+
+  void onItemDragged(int index) {
+    if (!_ctrlPressed && _clearCellAllowed) clearCell(index);
+
     _ctrlPressed = false;
-    _clearCellAllowed =  false;
+    _clearCellAllowed = false;
   }
-  
-  void clearCell(int index){
+
+  void clearCell(int index) {
     cells[index] = null;
   }
 }
 
 class TrackLineCell {
-  
+
   String sampleName;
   String href;
-  
+
   TrackLineCell(this.sampleName, this.href);
 
   TrackLineCell.fromJSON(String jsonString) {
@@ -67,7 +63,7 @@ class TrackLineCell {
     href = storedTrackLine['h'] == "null" ? '' : storedTrackLine['h'];
   }
 
-  String toJson(){
-    return  '{ "n": "$sampleName", "h": "$href" } '; 
+  String toJson() {
+    return '{ "n": "$sampleName", "h": "$href" } ';
   }
 }
