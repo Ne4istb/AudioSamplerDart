dcfa574533eb06926b7307d01612257e746c8b86
ne4istb <anechytailov@gmail.com>
2014-05-04 22:27:37 +0300

    Attempt 2 to fix server

--- bin/basic_http_server.dart
+++ bin/basic_http_server.dart
@@ -4,13 +4,13 @@ import 'package:http_server/http_server.dart' show VirtualDirectory;
 
 void main() {
   // Assumes the server lives in bin/ and that `pub build` ran.
-  var buildUri = Platform.script.resolve('../build/web');
+  var buildUri = Platform.script.resolve('../build');
 
   var staticFiles = new VirtualDirectory(buildUri.toFilePath());
   staticFiles
     ..allowDirectoryListing = true
     ..directoryHandler = (dir, request) {
-      var indexUri = new Uri.file(dir.path).resolve('audioSampler.html');
+      var indexUri = new Uri.file(dir.path).resolve('audioSampler2.html');
       var uri = indexUri.toFilePath();
       print(uri);
       staticFiles.serveFile(new File(uri), request);
