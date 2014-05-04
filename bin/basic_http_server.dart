ae3bcf081d1a092136e528d4c2503a7778ee8080
ne4istb <anechytailov@gmail.com>
2014-05-04 22:19:55 +0300

    Attempt to fix server

--- bin/basic_http_server.dart
+++ bin/basic_http_server.dart
@@ -1,27 +1,27 @@
-import 'dart:io' show File, HttpServer, Platform;
+import 'dart:io';
 import 'dart:async' show runZoned;
 import 'package:http_server/http_server.dart' show VirtualDirectory;
 
 void main() {
   // Assumes the server lives in bin/ and that `pub build` ran.
-  var buildUri = Platform.script.resolve('../build');
+  var buildUri = Platform.script.resolve('../build/web');
 
   var staticFiles = new VirtualDirectory(buildUri.toFilePath());
   staticFiles
-      ..allowDirectoryListing = true
-      ..directoryHandler = (dir, request) {
-    // Redirect directory-requests to piratebadge.html file.
-    var indexUri = new Uri.file(dir.path).resolve('audioSampler.html');
-    staticFiles.serveFile(new File(indexUri.toFilePath()), request);
-  };
+    ..allowDirectoryListing = true
+    ..directoryHandler = (dir, request) {
+      var indexUri = new Uri.file(dir.path).resolve('audioSampler.html');
+      var uri = indexUri.toFilePath();
+      print(uri);
+      staticFiles.serveFile(new File(uri), request);
+    };
 
   var portEnv = Platform.environment['PORT'];
   var port = portEnv == null ? 9999 : int.parse(portEnv);
 
   runZoned(() {
-    HttpServer.bind('0.0.0.0', port).then((server) {
+    HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, port).then((server) {
       server.listen(staticFiles.serveRequest);
     });
-  },
-  onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+  }, onError: (e, stackTrace) => print('Error: $e $stackTrace'));
 }
