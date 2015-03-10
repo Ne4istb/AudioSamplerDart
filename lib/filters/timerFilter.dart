8f40f69ee7738b0920769017f233a2e3b192521a
ne4istb <anechytailov@gmail.com>
2015-03-11 00:58:23 +0200

    Fixed timer filter

--- lib/filters/timerFilter.dart
+++ lib/filters/timerFilter.dart
@@ -1,3 +1,5 @@
+library timer_filter;
+
 import 'package:angular/angular.dart';
 import 'package:intl/intl.dart';
 
@@ -5,19 +7,24 @@ import 'package:intl/intl.dart';
 class TimerFilter {
 	call(num value) {
 
-		if (value == null)
-			return null;
+		if (value == null) return null;
+
+		if (value < 0)	return "00 : 00 : 000";
+
 
 		var formatter = new NumberFormat("00", "en_US");
 
-		int minutes = value ~/ 60;
+		var minutes = value ~/ 60;
 		String minutesStr = formatter.format(minutes);
 
-		String seconds = formatter.format(value - value % 1 - (minutes * 60));
+		var seconds = value - value % 1 - (minutes * 60);
+		String secondsStr = formatter.format(seconds);
 
 		formatter = new NumberFormat("000", "en_US");
-		String millis = formatter.format(value % 1 * 1000);
 
-		return "$minutesStr : $seconds : $millis";
+		var millis = value % 1 * 1000;
+		String millisStr = formatter.format(millis);
+
+		return "$minutesStr : $secondsStr : $millisStr";
 	}
 }
